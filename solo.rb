15a7ec462699c312748bda9e9ce4f6acda8fd5f8
Josh Mervine <jmervine@yp.com>
2013-03-17 23:01:39 -0700

    simplifying solo.rb

--- solo.rb
+++ solo.rb
@@ -1,13 +1,8 @@
-local = File.dirname(__FILE__)
-root  = "/tmp"
+root_directory = "/tmp/chef-solo"
 
-file_cache_path             File.expand_path("chef-tmp", root)
-data_bag_path               File.expand_path("chef-data_bags", root)
-encrypted_data_bag_secret   File.expand_path("data_bag_key", root)
-cookbook_path               [ File.expand_path("chef-site-cookbooks", root),
-                              File.expand_path("chef-cookbooks", root),
-                              File.expand_path("cookbooks", local) ]
-role_path                   File.expand_path("chef-roles", root)
+file_cache_path root_directory
+cookbook_path   [ File.expand_path("cookbooks", root_directory), File.expand_path("site-cookbooks", root_directory) ]
+
 
 # uncomment for more logging
 log_level :info
